d319f26d97552d5cf93ac4df8d3fb2b0fa7c54ae
bobby <parateakash21.ap@gmail.com>
2022-03-24 19:41:14 +0530

    Debug

--- config/settings.py
+++ config/settings.py
@@ -127,16 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+# STATIFILES_STORAGE = 'whitenoise.django.GzipMainfestStaticFilesStorage'
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media_root'
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
